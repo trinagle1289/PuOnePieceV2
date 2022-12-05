aa1883c5ee037d68b7022b847d45fddbcc7c0133
trinagle1289 <denny95090@gmail.com>
2022-12-05 13:19:08 +0800

    [feat] 完成作業
    修改 https://putriangle1289-2-default-rtdb.firebaseio.com/ 中 g1110190/led01 的資料，將其修改成 1

--- app/src/main/java/tw/edu/pu/g1110190/puonepiecev2/MapsActivity.java
+++ app/src/main/java/tw/edu/pu/g1110190/puonepiecev2/MapsActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import tw.edu.pu.g1110190.puonepiecev2.databinding.ActivityMapsBinding;
 
@@ -48,5 +50,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://putriangle1289-2-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = database.getReference("g1110190/led01");
+
+        myRef.setValue(1);
+
     }
 }
